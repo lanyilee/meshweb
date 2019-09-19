cb19cdb23eaac85ed8551473aafafd93af518cdc
lanyilee <2269146219@qq.com>
2019-09-20 07:37:06 +0800

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PandectComponent } from './pandect/pandect.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 
 const routes: Routes = [
-  { path: '', component: PandectComponent}
+  { path: '', component: PandectComponent},
+  { path: 'servicelist', component: ServiceListComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { PandectComponent } from './pandect/pandect.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       SideBarComponent,
-      PandectComponent
+      PandectComponent,
+      ServiceListComponent
    ],
    imports: [
       BrowserModule,
